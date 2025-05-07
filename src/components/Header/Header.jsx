d03a14f90c9ca3859a1779181966dbbee6da274c
Francisco Moreno <fmoreno@aligntech.com>
2025-05-07 17:33:55 +0200

    feature: step 2
    icons for tablet and phone

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import { COLORS, WEIGHTS, QUERIES } from '../../constants';
 import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
+import UnstyledButton from '../UnstyledButton';
+import Icon from '../Icon';
+
+const StyledMobileMenu = styled(MobileMenu)`
+  display: flex;
+  width: 100%;
+`;
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -30,9 +37,20 @@ const Header = () => {
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
         <Side />
+        <ButtonWrapper>
+        <UnstyledButton>
+          <Icon id="shopping-bag" strokeWidth={1} />
+        </UnstyledButton>
+          <UnstyledButton>
+            <Icon id="search" strokeWidth={1} />
+          </UnstyledButton>
+          <UnstyledButton onClick={() => setShowMobileMenu(true)}>
+            <Icon id="menu" strokeWidth={1} />
+          </UnstyledButton>
+          </ButtonWrapper>
       </MainHeader>
-
-      <MobileMenu
+      
+      <StyledMobileMenu
         isOpen={showMobileMenu}
         onDismiss={() => setShowMobileMenu(false)}
       />
@@ -41,6 +59,7 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
+  container-type: inline-size;
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
@@ -48,10 +67,21 @@ const MainHeader = styled.div`
   border-bottom: 1px solid ${COLORS.gray[300]};
 `;
 
+const ButtonWrapper = styled.nav`
+  display: none;
+  column-gap: 1rem;
+  @container ${QUERIES.tabletAndDown} {
+    display: flex;
+  }
+`;
+
 const Nav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
+  @container ${QUERIES.tabletAndDown} {
+    display: none;
+  }
 `;
 
 const Side = styled.div`
@@ -69,5 +99,7 @@ const NavLink = styled.a`
     color: ${COLORS.secondary};
   }
 `;
+
+
 
 export default Header;
