184f096a84017d7f6a5d965f116a3b1cd4e52b1b
Francisco Moreno <fmoreno@aligntech.com>
2025-05-20 15:10:42 +0200

    feature: step 3

--- src/components/ShoeIndex/ShoeIndex.jsx
+++ src/components/ShoeIndex/ShoeIndex.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -9,38 +9,46 @@ import Spacer from '../Spacer';
 import ShoeSidebar from '../ShoeSidebar';
 import ShoeGrid from '../ShoeGrid';
 
+const StyledSelect = styled(Select)``;
+
 const ShoeIndex = ({ sortId, setSortId }) => {
   return (
-    <Wrapper>
-      <MainColumn>
-        <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
-        </Header>
-        <Spacer size={32} />
-        <ShoeGrid />
-      </MainColumn>
-      <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
-        <Spacer size={42} />
-        <ShoeSidebar />
-      </LeftColumn>
-    </Wrapper>
+    <Container>
+      <Wrapper>
+        <MainColumn>
+          <Header>
+            <Title>Running</Title>
+            <StyledSelect
+              label="Sort"
+              value={sortId}
+              onChange={(ev) => setSortId(ev.target.value)}
+            >
+              <option value="newest">Newest Releases</option>
+              <option value="price">Price</option>
+            </StyledSelect>
+          </Header>
+          <Spacer size={32} />
+          <ShoeGrid />
+        </MainColumn>
+        <LeftColumn>
+          <Breadcrumbs>
+            <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+            <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+            <Breadcrumbs.Crumb href="/sale/shoes">
+              Shoes
+            </Breadcrumbs.Crumb>
+          </Breadcrumbs>
+          <Spacer size={42} />
+          <ShoeSidebar />
+        </LeftColumn>
+      </Wrapper>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  container-type: inline-size;
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -51,6 +59,9 @@ const Wrapper = styled.div`
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+  @container ${QUERIES.tabletAndDown} {
+    display: none;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -61,6 +72,11 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+  @container ${QUERIES.tabletAndDown} {
+    ${StyledSelect} {
+      display: none;
+    }
+  }
 `;
 
 const Title = styled.h2`
